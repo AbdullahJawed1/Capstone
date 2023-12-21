84bfa56389c5706aa23ae6eac919f09896efdcca
AbdullahJawed1 <82279451+AbdullahJawed1@users.noreply.github.com>
2023-12-21 16:01:02 +0500

    active tab now has blue color

--- Capstone/src/Components/NavBar/NavBar.jsx
+++ Capstone/src/Components/NavBar/NavBar.jsx
@@ -1,34 +1,48 @@
 // components/NavBar.js
 import React from "react";
 import "./NavBar.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import Projects from "../ProjectsPage/Projects.jsx";
 
 export default function NavBar() {
   return (
     <>
       <Navbar expand="lg" className="bg-body-tertiary">
-        <Container classsName="Navbar--">
+        <Container className="Navbar--">
           <Navbar.Brand
-            href="/"
+            as={NavLink}
+            to="/"
             style={{ color: "#3B71CA", fontWeight: "bold" }}
+            activeClassName=""
           >
             CAPSTONE
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link exact activeClassName='isActive' href="/">Home</Nav.Link>
-              <Nav.Link activeClassName='isActive' href="/ProjectsPage">Projects</Nav.Link>
-              <Nav.Link activeClassName='isActive' href="/GroupsPage">Groups</Nav.Link>
+              <Nav.Link as={NavLink} to="/" exact activeClassName="active">
+                Home
+              </Nav.Link>
+              <Nav.Link
+                as={NavLink}
+                to="/ProjectsPage"
+                activeClassName="active"
+              >
+                Projects
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/GroupsPage" activeClassName="active">
+                Groups
+              </Nav.Link>
               <NavDropdown title="Settings" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Chat</NavDropdown.Item>
+                <NavDropdown.Item as={NavLink} to="/profile">
+                  Profile
+                </NavDropdown.Item>
+                <NavDropdown.Item as={NavLink} to="/chat">
+                  Chat
+                </NavDropdown.Item>
                 <NavDropdown.Divider />
               </NavDropdown>
             </Nav>
