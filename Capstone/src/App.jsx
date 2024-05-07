92f2fc2dfa3e6783ea571effe444ae73439c55ec
abdullah <abdullah.yousuf115@gmail.com>
2024-05-08 03:17:09 +0500

    deleted old chat

--- Capstone/src/App.jsx
+++ Capstone/src/App.jsx
@@ -8,12 +8,10 @@ import ProjectsPage from "./Components/ProjectsPage/ProjectsPage";
 import GroupsPage from "./Components/GroupsPage/GroupsPage";
 import Profile from "./Components/Profle/Profile";
 import SupervisorsPage from "./Components/Supervisors/SupervisorsPage";
-import Chat from "./Components/Chat/Chat";
 import Proposal from "./Components/Proposal/ProposalPage";
 import StudentRegister from "./Components/StudentRegister";
 import SuperVisorRegister from "./Components/SuperVisorRegister";
 import Login from "./Components/Login";
-import ChatHome from "./Components/chatComponents/chatHome";
 import SupervisorProfile from "./Components/Supervisors/SupervisorProfile"
 import SendProposal from "./Components/Supervisors/SendProposal"
 import SimilarityChecker from "./Components/ProjectsPage/similarityChecker"
@@ -31,12 +29,10 @@ function App() {
           <Route path="/GroupsPage" element =  {<GroupsPage/>}>  </Route>
           <Route path="/Profile" element =  {<Profile/>}>  </Route>
           <Route path="/SupervisorsPage" element =  {<SupervisorsPage/>}>  </Route>
-          <Route path="/Chat" element =  {<ChatHome/>}>  </Route>
           <Route path="/xyz" element =  {<SuperVisorRegister/>}>  </Route>
           <Route path="/Register" element =  {<StudentRegister/>}>  </Route>
           <Route path="/Login" element =  {<Login/>}>  </Route>
           <Route path="/Proposal" element =  {<Proposal/>}>  </Route>
-          {/* <Route path="/Chat" element =  {<ChatHome/>}>  </Route> */}
           {/* <Route path="/SupervisorsPage/SupervisorProfile" element =  {<SupervisorProfile/>}>  </Route> */}
           <Route path="/SupervisorsPage/SupervisorProfile/:id" element={<SupervisorProfile />} />
           <Route path="/SupervisorsPage/SendProposal/:id" element =  {<SendProposal/>}>  </Route>
